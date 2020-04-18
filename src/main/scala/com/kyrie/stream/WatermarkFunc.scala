b30b4e8b306a1e3dde628195dc7e1ef1a3196479
changyue li <changyue.li@tendcloud.com>
2020-04-18 20:34:47 +0800

    flink

--- src/main/scala/com/kyrie/stream/WatermarkFunc.scala
+++ src/main/scala/com/kyrie/stream/WatermarkFunc.scala
@@ -4,9 +4,15 @@ import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows
 import org.apache.flink.streaming.api.windowing.time.Time
 
-
+/**
+ * 处理顺序的数据
+ * 如果我们事先得知数据流的时间戳是单调递增的，也就是说没有乱序，那我们可以使用 assignAscendingTimestamps，
+ * 这个方法会直接使用数据的时间戳生成 watermark。
+ */
+case class Feedback2(id:String,timestamp:Long,fre:Int)
 object WatermarkFunc {
 
   """
@@ -32,10 +38,31 @@ object WatermarkFunc {
     //设置为事件事件，原数据要包含timestamp字段，才能生效
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
 
-    val stream = env.readTextFile("data/src2.txt")
+    //设置检查水位线时间 ，默认200毫米
+    env.getConfig.setAutoWatermarkInterval(200)
+
+    env.setParallelism(1)
+
+    val stream =env.socketTextStream("localhost",9999)
+
+    val textWithTsDstream = stream.map{text =>
+      val arr:Array[String] = text.split(" ")
+      Feedback2(arr(0),arr(1).toLong,1)
+    }
+
+    //指定timestamp字段;秒转成毫秒
+    val textWithEventTimeStream = textWithTsDstream.assignAscendingTimestamps(e => e.timestamp *1000)
 
 
 
+    val keyStream = textWithEventTimeStream
+      .keyBy(_.id)
+    keyStream.print("keytext:")
+    val windowStream = keyStream
+      .window(SlidingEventTimeWindows.of(Time.seconds(10),Time.seconds(5)))
+
+
+    windowStream.reduce{(a,b) => (Feedback2(a.id,a.timestamp + b.timestamp, a.fre + b.fre))}.print("window:")
 
     env.execute()
 
