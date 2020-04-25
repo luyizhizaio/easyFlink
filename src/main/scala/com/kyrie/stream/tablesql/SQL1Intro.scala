443dac8df789fb36aa03407ca2028da34e769dbb
changyue li <changyue.li@tendcloud.com>
2020-04-25 22:17:05 +0800

    table api

--- src/main/scala/com/kyrie/stream/tablesql/SQL1Intro.scala
+++ src/main/scala/com/kyrie/stream/tablesql/SQL1Intro.scala
@@ -1,12 +1,10 @@
 package com.kyrie.stream.tablesql
 
-import com.kyrie.stream.watermark.Feedback2
 import org.apache.flink.table.api._
 import org.apache.flink.streaming.api.scala._
-import org.apache.flink.table.api.scala._
-import org.apache.flink.api.scala._
-
+case class Feedback3(id:String,timestamp:Long,fre:Int)
 object SQL1Intro {
+
   """
     |Table API 是流处理和批处理通用的关系型 API，
     |Table API 可以基于流输入或者批输入来运行而不需要进行任何修改。
@@ -20,12 +18,11 @@ object SQL1Intro {
 
     val stream = env.socketTextStream("192.168.0.104",9999,'\n')
 
-
     val tableEnv = TableEnvironment.getTableEnvironment(env)
 
-    val feedbackStream = stream.map{line =>
+    val feedbackStream:DataStream[Feedback3] = stream.map{line =>
       val Array(id,ts,num) = line.split(",")
-      Feedback2(id,ts.toLong,num.toInt)
+      Feedback3(id,ts.toLong,num.toInt)
     }
 
     //stream转table
@@ -40,20 +37,27 @@ object SQL1Intro {
 
     resultStream.print("result")
 
+    env.execute()
 
+  }
+
+  def tupleTable(stream:DataStream[String])={
+
+    import org.apache.flink.api.scala._
     /*********************************************/
 
-    val stream2:DataStream[(String,Long,Int)]= stream.map{line =>
+    val stream2:DataStream[(String,Long,Int)]= stream.map[(String,Long,Int)]{line =>
       val Array(id,ts,num) = line.split(",")
       (id,ts.toLong,num.toInt)
     }
 
     //转换元祖为table，指定字段名称
-   val tab3 = tableEnv.fromDataStream(stream2, 'id2, 'ts, 'fre)
+//    val tab3 = tableEnv.fromDataStream(stream2, 'id2, 'ts, 'fre)
+//
+//    tab3.select("id,fre").filter("id = 'id1'")
 
-    tab3.select("id,fre").filter("id = 'id1'")
 
-    env.execute()
+
 
   }
 
